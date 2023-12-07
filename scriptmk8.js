9b792a7f5ab2fd43c690969abf7dce3f187380fc
pcPixelz <86295592+pcPixelz@users.noreply.github.com>
2023-12-07 20:41:55 +0100

    Add files via upload

--- scriptmk8.js
+++ scriptmk8.js
@@ -3,3 +3,15 @@ var clickableArea = document.getElementById("row#11");
 clickableArea.addEventListener("click", function() {
     window.location.href = 'https://drive.google.com/uc?id=19ywAk-jH78y9gXczNITJaBy47ucHRbC-';
 });
+
+var clickableArea = document.getElementById("row#12");
+
+clickableArea.addEventListener("click", function() {
+    window.location.href = 'https://drive.google.com/uc?export=download&id=1fhmq-mjFDSsVtZhhrx_AljoJ4t1rRbiD';
+});
+
+var clickableArea = document.getElementById("row#13");
+
+clickableArea.addEventListener("click", function() {
+    window.location.href = 'https://drive.google.com/uc?export=download&id=11uNipCYhTl9iLTbwEs-YSIul5xVXkq8s';
+});
